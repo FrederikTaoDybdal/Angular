b29528ea58245eca38ae038b044a2fa9acbd469e
FrederikTao <frederiktaodybdallarsen@gmail.com>
2018-01-08 16:46:26 -0800

    Promises

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,22 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
 
 @Injectable()
 export class DishService {
 
   constructor() { }
 
-  getDishes(): Dish[] {
-    return DISHES;
+ getDishes(): Promise<Dish[]> {
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(DISHES), 1000);
+    });
   }
 
-    getDish(id: number): Dish {
-    return DISHES.filter((dish) => (dish.id === id))[0];
+  getDish(id: number): Promise<Dish> {
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 1000);
+    });
   }
 
-  getFeaturedDish(): Dish {
-    return DISHES.filter((dish) => dish.featured)[0];
+  getFeaturedDish(): Promise<Dish> {
+    return  new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 1000);
+    });
+  }
+
+   getDishIds(): Observable<number[]> {
+    return Observable.of(DISHES.map(dish => dish.id ));
   }
   
 }
